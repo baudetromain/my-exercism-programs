fc70a2a34b1a0e8cba0e2ee959eb6867794c26e8
romain baudet <romain74.baudet@gmail.com>
2022-06-27 15:00:30 +0200

    Advancement

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,18 +1,47 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::thread::JoinHandle;
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+pub fn frequency(input: &'static &[&str], worker_count: usize) -> HashMap<char, usize> {
     let counter: Arc<Mutex<HashMap<char, usize>>> = Arc::new(Mutex::new(HashMap::new()));
+    let len: Arc<usize> = Arc::new(input.len());
+    let data = Arc::new(input);
     let mut handles = vec![];
 
     for i in 0..worker_count
     {
         let counter = Arc::clone(&counter);
-        let handle = thread::spawn(||
+        let len = Arc::clone(&len);
+        let data = Arc::clone(&data);
+        let strings_to_handle = match input.len() % worker_count
+        {
+            0 => input.len() / worker_count,
+            _ => input.len() / worker_count + 1
+        };
+        let handle = thread::spawn(move ||
             {
-
+                for j in strings_to_handle * i .. (strings_to_handle * (i + 1))
+                {
+                    match j
+                    {
+                        j if j < *len =>
+                            {
+                                let _ = (*data).get(j)
+                                    .unwrap()
+                                    .chars()
+                                    .into_iter()
+                                    .map(|_char| {
+                                        let mut counter = counter.lock().unwrap();
+                                        match counter.get(&_char)
+                                        {
+                                            Some(amount) => counter.insert(_char, amount + 1),
+                                            None => counter.insert(_char, 1)
+                                        }
+                                    });
+                            },
+                        _ => {}
+                    }
+                }
             });
         handles.push(handle);
     }
