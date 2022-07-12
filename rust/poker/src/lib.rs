831b45f8c9db79e4f24c929bac5504e559adf0ac
Natsukooh <romain74.baudet@gmail.com>
2022-07-12 22:15:37 +0200

    Implemented poker hands

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,17 +1,53 @@
-use std::collections::HashMap;
-use std::ops::Deref;
 use std::str::FromStr;
+
+#[macro_use] extern crate maplit;
 
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum Value
 {
-    Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+    Two, Three, Four, Five, Six, Seven, Eight, Nine, Jack, Queen, King, Ace
 }
 
+impl FromStr for Value
+{
+    type Err = PokerHandFromStrConversionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err>
+    {
+        if s.len() != 1
+        {
+            return Err(PokerHandFromStrConversionError::ValueLenError);
+        }
+
+        let value_chars = hashmap!{
+            'A' => Value::Ace,
+            '2' => Value::Two,
+            '3' => Value::Three,
+            '4' => Value::Four,
+            '5' => Value::Five,
+            '6' => Value::Six,
+            '7' => Value::Seven,
+            '8' => Value::Eight,
+            '9' => Value::Nine,
+            'J' => Value::Jack,
+            'Q' => Value::Queen,
+            'K' => Value::King,
+        };
+
+        match value_chars.get(&s.chars().nth(0).unwrap()) // the unwrap is safe because we know the str length is 1
+        {
+            Some(color) => Ok(*color), // won't work without the Copy trait cause we would not be able to move the color out of the HashMap
+            None => Err(PokerHandFromStrConversionError::UnknownValue)
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum Color
 {
     Spade, Diamond, Heart, Club
@@ -28,20 +64,22 @@ impl FromStr for Color
             return Err(PokerHandFromStrConversionError::ColorLenError);
         }
 
-        let mut color_letters: HashMap<char, Color> = HashMap::new();
-        color_letters.insert('C', Color::Club);
-        color_letters.insert('D', Color::Diamond);
-        color_letters.insert('H', Color::Heart);
-        color_letters.insert('S', Color::Spade);
+        let color_chars = hashmap! {
+            'C' => Color::Club,
+            'D' => Color::Diamond,
+            'H' => Color::Heart,
+            'S' => Color::Spade,
+        };
 
-        match color_letters.get(&s.chars().nth(0).unwrap()) // the unwrap is safe because we know the str length is 2
+        match color_chars.get(&s.chars().nth(0).unwrap()) // the unwrap is safe because we know the str length is 2
         {
-            Some(color) => Ok(color),
+            Some(color) => Ok(*color), // won't work without the Copy trait cause we would not be able to move the color out of the HashMap
             None => Err(PokerHandFromStrConversionError::UnknownColor)
         }
     }
 }
 
+#[derive(PartialEq, Debug)]
 struct PokerCard
 {
     value: Value,
@@ -59,23 +97,29 @@ impl FromStr for PokerCard
             return Err(PokerHandFromStrConversionError::CardLenError);
         }
 
-
-
-        todo!()
+        Ok(PokerCard
+        {
+            value: Value::from_str(s.chars().nth(0).unwrap().to_string().as_str())?,
+            color: Color::from_str(s.chars().nth(1).unwrap().to_string().as_str())?,
+        })
     }
 }
 
-struct PokerHand<'a>(Vec<&'a PokerCard>);
+#[derive(PartialEq, Debug)]
+struct PokerHand(Vec<PokerCard>);
 
+#[derive(PartialEq, Debug)]
 enum PokerHandFromStrConversionError
 {
     CardsAmountError,
     CardLenError,
     UnknownColor,
-    ColorLenError
+    UnknownValue,
+    ColorLenError,
+    ValueLenError
 }
 
-impl FromStr for PokerHand<'_>
+impl FromStr for PokerHand
 {
     type Err = PokerHandFromStrConversionError;
 
@@ -88,10 +132,80 @@ impl FromStr for PokerHand<'_>
             return Err(PokerHandFromStrConversionError::CardsAmountError);
         }
 
-
-
-        Ok(PokerHand(vec![]))
+        split_str.iter()
+            .map(|str_card_rep| PokerCard::from_str(str_card_rep))
+            .collect::<Result<Vec<PokerCard>, PokerHandFromStrConversionError>>()
+            .map(|vec| PokerHand(vec))
     }
+}
+
+#[test]
+fn basic_success()
+{
+    assert_eq!(
+        "2S 4S 7H AC JH".parse::<PokerHand>(),
+        Ok(PokerHand{ 0: vec![
+            PokerCard { color: Color::Spade, value: Value::Two },
+            PokerCard { color: Color::Spade, value: Value::Four },
+            PokerCard { color: Color::Heart, value: Value::Seven },
+            PokerCard { color: Color::Club, value: Value::Ace },
+            PokerCard { color: Color::Heart, value: Value::Jack },
+        ] })
+    );
+}
+
+#[test]
+fn empty()
+{
+    assert_eq!(
+        "".parse::<PokerHand>(),
+        Err(PokerHandFromStrConversionError::CardsAmountError)
+    )
+}
+
+#[test]
+fn not_enough_cards()
+{
+    assert_eq!(
+        "2S 4S 7H AC".parse::<PokerHand>(),
+        Err(PokerHandFromStrConversionError::CardsAmountError)
+    )
+}
+
+#[test]
+fn too_big_card()
+{
+    assert_eq!(
+        "2S 4S 7H AC JHH".parse::<PokerHand>(),
+        Err(PokerHandFromStrConversionError::CardLenError)
+    )
+}
+
+#[test]
+fn too_small_card()
+{
+    assert_eq!(
+        "2S 4S 7H AC J".parse::<PokerHand>(),
+        Err(PokerHandFromStrConversionError::CardLenError)
+    )
+}
+
+#[test]
+fn wrong_color()
+{
+    assert_eq!(
+        "2S 4S 7H AC JX".parse::<PokerHand>(),
+        Err(PokerHandFromStrConversionError::UnknownColor)
+    )
+}
+
+#[test]
+fn wrong_value()
+{
+    assert_eq!(
+        "2S 4S 7H AC ZH".parse::<PokerHand>(),
+        Err(PokerHandFromStrConversionError::UnknownValue)
+    )
 }
 
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str>
